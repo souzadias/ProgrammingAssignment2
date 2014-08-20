bd0af60deee9a0b432664300915da692415d02f1
souzadias <souzadias@users.noreply.github.com>
2014-08-20 09:59:21 +0200

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,59 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Function provided in exercise
+##
+##makeVector <- function(x = numeric()) {
+##    m <- NULL
+##    set <- function(y) {
+##        x <<- y
+##        m <<- NULL
+##    }
+##    get <- function() x
+##    setmean <- function(mean) m <<- mean
+##   getmean <- function() m
+##    list(set = set, get = get,
+##         setmean = setmean,
+##         getmean = getmean)
+##}
+## makeVector()
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+## Modified function according to exercise
+makeCacheMatrix <- function (x = matrix()) {
+    m <- null
+    set <- function(y) {
+        x <<- y
+        m <<- NULL
+    }
+    get <- function() x
+    setsolve <- function(solve) m <<- solve
+    getsolve <- function() m
+    list(set =set, get = get,
+         setsolve = setsolve,
+         getsolve = getsolve)
 }
 
 
-## Write a short comment describing this function
+## Function provided in exercise
+##
+##cachemean <- function(x, ...) {
+##    m <- x$getmean()
+##    if(!is.null(m)) {
+##        message("getting cached data")
+##        return(m)
+##    }
+##    data <- x$get()
+##    m <- mean(data, ...)
+##    x$setmean(m)
+##    m
+##}
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+## Modified function according to exercise
+cachesolve <- function(x, ...) {
+    m <-  x$getsolve(
+        if (!is.null(m)) {
+            message ("getting cached data")
+            return (m)
+        }
+        data <- x$get()
+        m <- solve(data, ...)
+        x$setsolve(m)
+        m    
 }
